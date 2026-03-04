8e23a7240d2ee9f0f8d916f74ea85b00bbeb106a
pagetableK <pablo14gamerx3@gmail.com>
2026-03-03 19:24:44 -0600

    Cambio de atributo: area (Persona) -> area (Usuario)

--- src/com/esen/tickets/Persona.java
+++ src/com/esen/tickets/Persona.java
@@ -3,12 +3,10 @@ package com.esen.tickets;
 public class Persona {
     public String nombre;
     public String correo;
-    public String area;
 
-    public Persona(String nombre, String correo, String area){
+    public Persona(String nombre, String correo){
         this.nombre = nombre;
         this.correo = correo;
-        this.area = area;
     }
 
 }
--- src/com/esen/tickets/Usuario.java
+++ src/com/esen/tickets/Usuario.java
@@ -1,4 +1,10 @@
 package com.esen.tickets;
 
-public class Usuario {
+public class Usuario extends Persona {
+    public String area;
+
+    public Usuario(String nombre, String correo, String area){
+        super(nombre, correo);
+        this.area = area;
+    }
 }
