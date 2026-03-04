8eb26a7f8405e7f9b2e06cfe6131f8a3babbacdc
LinkG475 <20245365@esen.edu.sv>
2026-03-03 19:39:26 -0600

    Atributo de rol a Usuario

--- src/com/esen/tickets/Usuario.java
+++ src/com/esen/tickets/Usuario.java
@@ -2,9 +2,11 @@ package com.esen.tickets;
 
 public class Usuario extends Persona {
     public String area;
+    public String rol;
 
-    public Usuario(String nombre, String correo, String area){
+    public Usuario(String nombre, String correo, String area, String rol){
         super(nombre, correo);
         this.area = area;
+        this.rol = rol;
     }
 }
