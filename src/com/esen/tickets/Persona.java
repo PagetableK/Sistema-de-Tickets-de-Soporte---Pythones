4ef93735c2f767926787c50ec92baf5c4d139370
LinkG475 <20245365@esen.edu.sv>
2026-03-03 19:01:53 -0600

    Primer commit

--- src/com/esen/tickets/Persona.java
+++ src/com/esen/tickets/Persona.java
@@ -1,4 +1,14 @@
 package com.esen.tickets;
 
 public class Persona {
+    String nombre;
+    String correo;
+    String area;
+
+    public Persona(String nombre, String correo, String area){
+        this.nombre = nombre;
+        this.correo = correo;
+        this.area = area;
+    }
+
 }
