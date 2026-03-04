960e09177749954c45c64c5cc664ed6e1fffa6e1
DMoralGar <donbidmorales@gmail.com>
2026-03-03 18:57:53 -0600

    Especialidad

--- src/com/esen/tickets/Tecnico.java
+++ src/com/esen/tickets/Tecnico.java
@@ -1,4 +1,11 @@
 package com.esen.tickets;
 
-public class Tecnico {
+public class Tecnico extends Persona {
+
+    public String especialidad;
+
+    public Tecnico(String nombre, String correo, String especialidad){
+        super(nombre,correo);
+        this.especialidad = especialidad;
+    }
 }
